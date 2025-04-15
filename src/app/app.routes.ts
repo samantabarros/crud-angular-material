4f5edd1f07f30d378f2dc4e643c75ce926dfbf22
samantabarros <samantadesenvolvedora@gmail.com>
2025-04-15 00:16:36 -0300

    feat:  Adiciona router link e  marerial Icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ConsultaComponent } from './consulta/consulta.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'consulta', component: ConsultaComponent},
+    {path: 'cadastro', component: CadastroComponent}
+];
