f4232f6b458e8aa6572b4243a1d776fa035cfd30
samantativic <samanta@tivic.com.br>
2025-04-16 17:50:27 -0300

    feat: Adiciona estrutura do formulário de cadastro de clientes

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    FormsModule, 
+    FlexLayoutModule, 
+    MatCardModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
