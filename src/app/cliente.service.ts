cdf0a7ca9e185d6f87fcfc0fc8bdb795369eea83
samantabarros <samantadesenvolvedora@gmail.com>
2025-04-17 00:36:31 -0300

    feat: Adiciona service de clientes

---
+++ src/app/cliente.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from './cadastro/cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor() { }
+  salvar(cliente: Cliente){
+    console.log(cliente);
+  }
+}
